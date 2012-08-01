134bc24b31d9320fe9c3efc63f496777e934be2e
KOLANICH <kolan_n@mail.ru>
2012-08-02 00:09:14 +0400

    Significant improvements. Code style and performance improved.

--- DropboxUploader.php
+++ DropboxUploader.php
@@ -3,6 +3,7 @@
  * Dropbox Uploader
  * 
  * Copyright (c) 2009 Jaka Jancar
+ * Copyright (c) 2012 KOLANICH
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -22,9 +23,17 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  *
- * @author Jaka Jancar [jaka@kubje.org] [http://jaka.kubje.org/]
+ * @author Jaka Jancar [jaka@kubje.org] [http://jaka.kubje.org/], improved and fixed by KOLANICH
  * @version 1.1.5
  */
+ 
+/*function getFileNumberByHandle($fh){
+	$txt="";
+	$txt.=$fh;
+	if(preg_match("%Resource id #(\d+)%i",$txt,$r)){
+		return $r[1];
+	};
+}*/
 class DropboxUploader {
     protected $email;
     protected $password;
@@ -34,8 +43,9 @@ class DropboxUploader {
     const CACERT_SOURCE_DIR = 2;
     protected $caCertSource;
     protected $loggedIn = false;
-    protected $cookies = array();
-    
+    protected $ch;
+    static $cookieFile;
+    static $certFile;
     /**
      * Constructor
      *
@@ -46,100 +56,120 @@ class DropboxUploader {
         // Check requirements
         if (!extension_loaded('curl'))
             throw new Exception('DropboxUploader requires the cURL extension.');
-        
-        $this->email = $email;
+		if(empty($email)||empty($password))throw new Exception('Some info needed to log in missed');
+		$this->email = $email;
         $this->password = $password;
+		static::initCh();
+		static::setCaCertificateFile(static::$certFile);
+		
+        
     }
     
+	function _destruct(){
+		curl_close($this->ch);
+	}
+	
+	function initCh(){
+		$this->ch = curl_init();
+        curl_setopt($this->ch, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, 1);
+		/*curl_setopt($this->ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($this->ch, CURLOPT_PROXY, '127.0.0.1:8888');*/
+		$cfile=str_replace("%MAIL%",$this->email,static::$cookieFile);
+        curl_setopt($this->ch, CURLOPT_COOKIEFILE,$cfile);
+        curl_setopt($this->ch, CURLOPT_COOKIEJAR,$cfile);
+        curl_setopt($this->ch, CURLOPT_HEADER, 1);
+        curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($this->ch, CURLOPT_FOLLOWLOCATION, 0);
+        curl_setopt($this->ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 5.1; rv:14.0) Gecko/20100101 Firefox/14.0.1');
+	}
+	
     public function setCaCertificateFile($file)
     {
         $this->caCertSourceType = self::CACERT_SOURCE_FILE;
         $this->caCertSource = $file;
+		curl_setopt($this->ch, CURLOPT_CAINFO, $this->caCertSource);
     }
     
     public function setCaCertificateDir($dir)
     {
         $this->caCertSourceType = self::CACERT_SOURCE_DIR;
         $this->caCertSource = $dir;
+		curl_setopt($this->ch, CURLOPT_CAPATH, $this->caCertSource);
     }
 
-    public function upload($source, $remoteDir='/', $remoteName=null) {
-        if (!file_exists($source) or !is_file($source) or !is_readable($source))
+    public function uploadBuffer($source, $remoteDir='/', $remoteName=null) {
+       $fh=fopen('php://memory',"w");
+		fwrite($fh,$source);
+		fflush($fh);
+        return static::upload('php://memory', $remoteDir, $remoteName);
+		fclose($fh);
+    }
+	
+	public function upload($source, $remoteDir='/', $remoteName) {
+		if (!file_exists($source) or !is_file($source) or !is_readable($source))
             throw new Exception("File '$source' does not exist or is not readable.");
-
-        if (!is_string($remoteDir))
-          throw new Exception("Remote directory must be a string, is ".gettype($remoteDir)." instead.");
-
-        if (is_null($remoteName)) {
+		if (is_null($remoteName)) {
             $remoteName = $source;
-        } else if (!is_string($remoteName)) {
+        }
+		if (!is_string($remoteName)) {
             throw new Exception("Remote filename must be a string, is ".gettype($remoteDir)." instead.");
         }
+        if (!is_string($remoteDir))
+          throw new Exception("Remote directory must be a string, is ".gettype($remoteDir)." instead.");
         
         if (!$this->loggedIn)
             $this->login();
         
-        $data = $this->request('https://www.dropbox.com/home');
-        $token = $this->extractToken($data, 'https://dl-web.dropbox.com/upload');
+        $res = $this->request('https://www.dropbox.com/home');
+        $token = $this->extractToken($res->data, 'https://dl-web.dropbox.com/upload');
 
-
+		
         $postdata = array('plain'=>'yes', 'file'=>'@'.$source.';filename='.$remoteName, 'dest'=>$remoteDir, 't'=>$token);
-        $data = $this->request('https://dl-web.dropbox.com/upload', true, $postdata);
-        if (strpos($data, 'HTTP/1.1 302 FOUND') === false)
+        //$postdata = array('plain'=>'yes', 'file'=>array('name' => $remoteName, 'file' =>$source), 'dest'=>$remoteDir, 't'=>$token);
+		curl_setopt($this->ch, CURLOPT_REFERER, 'https://www.dropbox.com/home');
+        $res = $this->request('https://dl-web.dropbox.com/upload', $postdata);
+       if (strpos($res->data, 'HTTP/1.1 302 FOUND') === false)
             throw new Exception('Upload failed!');
+		/*if ($res->code!=302)
+            throw new Exception('Upload failed!');*/
     }
     
-    protected function login() {
-        $data = $this->request('https://www.dropbox.com/login');
-        
-        $data = $this->request('https://www.dropbox.com/login', true, array('login_email'=>$this->email, 'login_password'=>$this->password));
-        
-        if (stripos($data, 'location: /home') === false)
-            throw new Exception('Login unsuccessful.');
+    public function login() {
+		curl_setopt($this->ch, CURLOPT_REFERER, 'https://www.dropbox.com/');
+        ///$res = $this->request('https://www.dropbox.com/login');
+        $res = $this->request('https://www.dropbox.com/login', array(
+			'login_email'=>$this->email,
+			'login_password'=>$this->password,
+			'remember_me'=>'on',
+			//'cont'=>'/home'
+		));
+        if (stripos($res->data, 'location: /home') === false)
+            throw new Exception('Login unsuccessful.: CURL error: '.$res->error.' Code '.$res->code);
         
         $this->loggedIn = true;
     }
+	
+    protected function request($url, $postData=null) {
+		curl_setopt($this->ch, CURLOPT_URL, $url);
+		//var_dump($postData);
+		if ($postData) {
+			curl_setopt($this->ch, CURLOPT_POST, 1);
+			//curl_setopt($this->ch, CURLOPT_POSTFIELDS, http_build_query($postData));
+			curl_setopt($this->ch, CURLOPT_POSTFIELDS, $postData);
+		}else{
+			curl_setopt($this->ch,CURLOPT_HTTPGET,1);
+		}
+		if ($resobj->data === false)
+			throw new Exception('Cannot execute request: '.curl_error($this->ch));
 
-    protected function request($url, $post=false, $postData=array()) {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
-        switch ($this->caCertSourceType) {
-            case self::CACERT_SOURCE_FILE:
-                curl_setopt($ch, CURLOPT_CAINFO, $this->caCertSource);
-                break;
-            case self::CACERT_SOURCE_DIR:
-                curl_setopt($ch, CURLOPT_CAPATH, $this->caCertSource);
-                break;
-        }
-        curl_setopt($ch, CURLOPT_HEADER, 1);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        if ($post) {
-            curl_setopt($ch, CURLOPT_POST, $post);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
-        }
-        
-        // Send cookies
-        $rawCookies = array();
-        foreach ($this->cookies as $k=>$v)
-            $rawCookies[] = "$k=$v";
-        $rawCookies = implode(';', $rawCookies);
-        curl_setopt($ch, CURLOPT_COOKIE, $rawCookies);
-        
-        $data = curl_exec($ch);
-        
-        if ($data === false)
-            throw new Exception('Cannot execute request: '.curl_error($ch));
-        
-        // Store received cookies
-        preg_match_all('/Set-Cookie: ([^=]+)=(.*?);/i', $data, $matches, PREG_SET_ORDER);
-        foreach ($matches as $match)
-            $this->cookies[$match[1]] = $match[2];
-        
-        curl_close($ch);
-        
-        return $data;
+
+		$resobj=new StdClass;
+		$resobj->data=curl_exec($this->ch);
+		$resobj->error=curl_error($this->ch);
+		$resobj->code=curl_errno($this->ch);
+		return $resobj;
     }
 
     protected function extractToken($html, $formAction) {
@@ -149,3 +179,6 @@ class DropboxUploader {
     }
 
 }
+DropboxUploader::$cookieFile=__DIR__.'/cookie_%MAIL%_.txt';
+DropboxUploader::$certFile=__DIR__.'/dropbox.crt';
+//var_dump(DropboxUploader::$cookieFile);
