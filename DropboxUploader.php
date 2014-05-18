db167fc7306b20ce48dd7d10050a7e58bc206df0
hakre <hanskrentel@yahoo.de>
2014-05-19 00:09:20 +0200

    Version bump v1.1.17
    The readme makes more clear now that this class is just not stable to
    use compared with the Dropbox API.

--- DropboxUploader.php
+++ DropboxUploader.php
@@ -23,7 +23,7 @@
  * THE SOFTWARE.
  *
  * @author Jaka Jancar <jaka@kubje.org> <http://jaka.kubje.org/>
- * @version 1.1.16
+ * @version 1.1.17
  * @license MIT <http://spdx.org/licenses/MIT>
  */
 final class DropboxUploader {
