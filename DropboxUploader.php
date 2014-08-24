e0e5407f409f36e61baaf4ae0305983787781d0a
hakre <hanskrentel@yahoo.de>
2014-08-24 21:24:33 +0200

    Version bump v1.1.19
    - Fix file modifcation time on upload. See jakajancar/DropboxUploader#40
    - Fix logout when done.
    - New testuite.
    - New composer.

--- DropboxUploader.php
+++ DropboxUploader.php
@@ -23,7 +23,7 @@
  * THE SOFTWARE.
  *
  * @author Jaka Jancar <jaka@kubje.org> <http://jaka.kubje.org/>
- * @version 1.1.18
+ * @version 1.1.19
  * @license MIT <http://spdx.org/licenses/MIT>
  */
 final class DropboxUploader {
