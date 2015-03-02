133b6761be11aa913c632582818c56bbba789b69
hakre <hanskrentel@yahoo.de>
2015-03-03 00:18:37 +0100

    Version bump v1.1.22
    - Fix Dropbox changed login response. See jakajancar/DropboxUploader#52
    - Minor CS fixes

--- DropboxUploader.php
+++ DropboxUploader.php
@@ -23,7 +23,7 @@
  * THE SOFTWARE.
  *
  * @author Jaka Jancar <jaka@kubje.org> <http://jaka.kubje.org/>
- * @version 1.1.21
+ * @version 1.1.22
  * @license MIT <http://spdx.org/licenses/MIT>
  */
 final class DropboxUploader {
